66cc2897f9a28ca87d38f751157c874a4dbc3e14
Diego França <diego.franca92@gmail.com>
2023-07-02 14:09:53 +0000

    feat: include angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent, CardComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, MatCardModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
